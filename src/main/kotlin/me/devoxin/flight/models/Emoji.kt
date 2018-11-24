d0e5c44c149d5014971b314049800b885d13e626
Devoxin <lukel9914@gmail.com>
2018-11-24 23:15:33 +0000

    Add URL to emoji

--- src/main/kotlin/me/devoxin/flight/models/Emoji.kt
+++ src/main/kotlin/me/devoxin/flight/models/Emoji.kt
@@ -2,4 +2,12 @@ package me.devoxin.flight.models
 
 class Emoji(public val name: String,
             public val id: Long,
-            public val animated: Boolean)
+            public val animated: Boolean) {
+
+    val url: String
+            get() {
+                val extension = if (animated) "gif" else "png"
+                return "https://cdn.discordapp.com/emojis/$id.$extension"
+            }
+
+}
