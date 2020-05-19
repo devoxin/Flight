16b8e0ddf637da5b228ed3d088b1a3a2030f742a
Devoxin <lukel9914@gmail.com>
2020-05-19 17:55:21 +0100

    Fix snowflake parser

--- src/main/kotlin/me/devoxin/flight/internal/arguments/types/Snowflake.kt
+++ src/main/kotlin/me/devoxin/flight/internal/arguments/types/Snowflake.kt
@@ -1,4 +1,4 @@
 package me.devoxin.flight.internal.arguments.types
 
-class Snowflake
+class Snowflake(val resolved: Long)
 // Exists solely for the snowflake parser.
--- src/main/kotlin/me/devoxin/flight/internal/parsers/SnowflakeParser.kt
+++ src/main/kotlin/me/devoxin/flight/internal/parsers/SnowflakeParser.kt
@@ -1,17 +1,18 @@
 package me.devoxin.flight.internal.parsers
 
 import me.devoxin.flight.api.Context
+import me.devoxin.flight.internal.arguments.types.Snowflake
 import java.util.*
 import java.util.regex.Pattern
 
-class SnowflakeParser : Parser<Long> {
+class SnowflakeParser : Parser<Snowflake> {
 
-    override fun parse(ctx: Context, param: String): Optional<Long> {
+    override fun parse(ctx: Context, param: String): Optional<Snowflake> {
         val match = snowflakeMatch.matcher(param)
 
         if (match.matches()) {
             val id = match.group("sid") ?: match.group("id")
-            return Optional.of(id.toLong())
+            return Optional.of(Snowflake(id.toLong()))
         }
 
         return Optional.empty()
