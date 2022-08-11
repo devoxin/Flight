41b87a8ccd4c77fd5926e94b0833cff4b33521e4
Devoxin <lukel9914@gmail.com>
2022-08-11 17:47:29 +0100

    Add reply handling to SlashContext

--- src/main/kotlin/me/devoxin/flight/api/context/Context.kt
+++ src/main/kotlin/me/devoxin/flight/api/context/Context.kt
@@ -2,6 +2,7 @@ package me.devoxin.flight.api.context
 
 import me.devoxin.flight.api.CommandClient
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.MessageBuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.MessageChannel
@@ -13,13 +14,13 @@ interface Context {
 
     /**
      * The current Context instance as a MessageContext instance,
-     * if contextType is ContextType.MESSAGE, otherwise null.
+     * if contextType is [ContextType.MESSAGE], otherwise null.
      */
     val asMessageContext: MessageContext?
         get() = this as? MessageContext
     /**
      * The current Context instance as a SlashContext instance,
-     * if contextType is ContextType.SLASH, otherwise null.
+     * if contextType is [ContextType.SLASH], otherwise null.
      */
     val asSlashContext: SlashContext?
         get() = this as? SlashContext
@@ -32,10 +33,8 @@ interface Context {
     val messageChannel: MessageChannel
 
     fun respond(content: String): CompletableFuture<*> {
-        val sendable = asSlashContext?.event?.reply(content)
-            ?: messageChannel.sendMessage(content)
-
-        return sendable.submit()
+        return asSlashContext?.respond0(MessageBuilder(content).build())
+            ?: messageChannel.sendMessage(content).submit()
     }
 
     fun send(content: String) {
--- src/main/kotlin/me/devoxin/flight/api/context/SlashContext.kt
+++ src/main/kotlin/me/devoxin/flight/api/context/SlashContext.kt
@@ -1,8 +1,14 @@
 package me.devoxin.flight.api.context
 
+import kotlinx.coroutines.future.await
 import me.devoxin.flight.api.CommandClient
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.InteractionHook
+import net.dv8tion.jda.api.interactions.components.Modal
+import java.util.concurrent.CompletableFuture
 
 class SlashContext(
     override val commandClient: CommandClient,
@@ -14,4 +20,81 @@ class SlashContext(
     override val guild = event.guild
     override val member = event.member
     override val messageChannel = event.channel
+
+    var replied = false
+        private set
+    var deferred = false
+        private set
+
+    fun defer(ephemeral: Boolean = false) {
+        if (!deferred) { // Idempotency handling
+            event.deferReply(ephemeral).queue { deferred = true }
+        }
+    }
+
+    suspend fun deferAsync(ephemeral: Boolean): InteractionHook {
+        if (!deferred) { // Idempotency handling
+            return event.deferReply(ephemeral).submit()
+                .thenApply { deferred = true; it }
+                .await()
+        }
+
+        return event.hook
+    }
+
+    fun reply(content: String, ephemeral: Boolean = false) {
+        respond(MessageBuilder(content).build(), ephemeral)
+    }
+
+    fun reply(modal: Modal) {
+        event.replyModal(modal).queue { replied = true }
+    }
+
+    /**
+     * This will only call [SlashCommandInteractionEvent.reply], with no
+     * special handling to account for acknowledged events.
+     */
+    fun reply(message: Message, ephemeral: Boolean = false) {
+        event.reply(message).setEphemeral(ephemeral).queue { replied = true }
+    }
+
+    /**
+     * This will only call [SlashCommandInteractionEvent.reply], with no
+     * special handling to account for acknowledged events.
+     */
+    suspend fun replyAsync(message: Message, ephemeral: Boolean = false): InteractionHook {
+        return event.reply(message).setEphemeral(ephemeral).submit().thenApply { replied = true; it }.await()
+    }
+
+    /**
+     * This will only call [InteractionHook.sendMessage] with no special handling.
+     */
+    fun send(message: Message, ephemeral: Boolean = false) {
+        event.hook.sendMessage(message).setEphemeral(ephemeral).queue()
+    }
+
+    /**
+     * This will only call [InteractionHook.sendMessage] with no special handling.
+     */
+    suspend fun sendAsync(message: Message, ephemeral: Boolean = false): Message {
+        return event.hook.sendMessage(message).setEphemeral(ephemeral).submit().await()
+    }
+
+    /**
+     * Convenience method which handles replying the correct way for you.
+     *
+     * The [ephemeral] setting is ignored if the interaction is deferred.
+     * Instead, the ephemeral setting when deferring is used. This is a Discord limitation.
+     */
+    fun respond(message: Message, ephemeral: Boolean = false) {
+        respond0(message, ephemeral)
+    }
+
+    internal fun respond0(message: Message, ephemeral: Boolean = false): CompletableFuture<*> {
+        return when {
+            replied -> event.hook.sendMessage(message).setEphemeral(ephemeral).submit()
+            deferred -> event.hook.editOriginal(message).submit().thenApply { replied = true }
+            else -> event.reply(message).setEphemeral(ephemeral).submit().thenApply { replied = true }
+        }
+    }
 }
